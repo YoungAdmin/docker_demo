eb7ca63086c58cc10f2255d62e4ede65972cfff6
qq943 <943001713@qq.com>
2020-12-07 19:00:12 +0800

    Initial commit

---
+++ src/main/java/com/zhg/docker/docker_demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.zhg.docker.docker_demo.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试接口
+ *
+ * @author qq943
+ * @date 2020/12/07 18:43
+ **/
+@RestController
+@Log4j2
+public class HelloController {
+
+    @GetMapping(value = "hello")
+    public void hello(){
+        System.out.println("hello!");
+        log.info("hello-log1");
+    }
+}
